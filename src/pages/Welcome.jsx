ebf6eff7a696a751648cedbe069154799dd349b6
amagolden <amagoldenstein@gmail.com>
2024-08-29 21:51:18 -0400

    updated homepage

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -9,9 +9,31 @@ function WelcomePage() {
     <AppNavbar />
       <div className="polls-page">
         <h1>Welcome!</h1>
+        <div className="p-3 mb-2 bg-light text-grey">
+          <p>"Does anyone have any questions?".... *crickets*</p>
+          <p>Crickets are the sound of <b>disengaged</b> employees. We're on a mission to improve team communication and <b>elimate crickets</b>.</p>
+        </div>
+        <h3>Manage Communications</h3>
         <p>
             <button 
-                className='form-button btn btn-success'
+                className='form-button btn btn-success homepage'
+                type='submit'
+                onClick={() => window.location.href='/allhands'} 
+                >Schedule an All Hands
+            </button>
+        </p>
+        <p>
+            <button 
+                className='form-button btn btn-success homepage'
+                type='submit'
+                onClick={() => window.location.href='/polls'} 
+                >Create a Poll
+            </button>
+        </p>
+        <h3>Latest Team Updates</h3>
+        <p>
+            <button 
+                className='form-button btn btn-success homepage'
                 type='submit'
                 onClick={() => window.location.href='/allhands'} 
                 >Latest All Hands
@@ -19,7 +41,7 @@ function WelcomePage() {
         </p>
         <p>
             <button 
-                className='form-button btn btn-success'
+                className='form-button btn btn-success homepage'
                 type='submit'
                 onClick={() => window.location.href='/polls'} 
                 >Latest Poll
